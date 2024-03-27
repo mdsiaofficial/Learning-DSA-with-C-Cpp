2d93ef2f7468ce35856e50cf459db97174b0122e
mdsiaofficial <mdsiaofficial@gmail.com>
2024-03-28 04:53:43 +0600

    bubble sort

---
+++ tamim_shahriar_subeen/6_bubble_sort.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#include <stdlib.h>
+
+#define nl endl
+#define nll cout<<endl
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define PLL pair<ll,ll>
+#define pii pair<int,int>
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) (x & (1LL << k))
+#define scn(n) scanf("%d",&n)
+#define scnll(n) scanf("%lld",&n)
+#define YES cout<<"YES\n"
+#define Yes cout<<"Yes\n"
+#define yes cout<<"yes\n"
+#define NO cout<<"NO\n"
+#define No cout<<"No\n"
+#define no cout<<"no\n"
+#define mod 1000000007LL
+#define mod1 1000000007LL
+#define mod2 1000000009LL
+#define inf 1000000000000000LL
+#define N 200000
+#define pi 3.14159
+
+// #define forn(i, n) for(int i = int(i); i < int(n); i++) 
+// not efficient
+
+#define mod9 90000007
+#define fs(n) fixed<<setprecision(int(n))
+#define s(n) setprecision(int(n))
+
+
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+//  g++ temp.cpp -o temp
+//          or
+//  g++ temp.cpp -o temp.exe
+//  .\temp.exe
+
+
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    nll;
+}
+int main(){
+    fastio;
+    
+    int arr[17] = {6,8,1,9,2,6,2,3,4,8,1,5,3,9,5,6,7};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, len);
+    // O(n^2);
+
+    return 0;
+}
