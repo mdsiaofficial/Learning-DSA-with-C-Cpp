de699d19e808966f9d30198fb93ab06d55173245
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-02 23:58:25 +0600

    hash

--- Jacob_Sorber/hash_table.c
+++ Jacob_Sorber/hash_table.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include<stdbool.h>
 
 #define nl printf("\n");
 
@@ -39,38 +39,106 @@
 //  .\temp.exe
 
 
+// Define the maximum length of a person's name
 #define MAX_NAME 256
-#define TABLE_SIZE 10
+// Define the size of the hash table
+#define TABLE_SIZE 20
 
+// Define the structure of a person
 typedef struct{
     char name[MAX_NAME];
     int age;
-    //...others here
+    //...other attributes here
 }person;
 
+// Initialize the hash table with the size defined above
 person *hash_table[TABLE_SIZE];
 
+// Define the hash function for a person's name
 ull hash(char *name){
+    // Get the length of the name
     int length = strnlen(name, MAX_NAME);
+    // Initialize the hash value
     ull hash_value = 0;
+    // Iterate over each character in the name
     for(int i=0; i<length; i++){
-        hash_value+=name[i]*19;
-        hash_value =(1+((hash_value/20) * name[i]) + MAX_NAME )%TABLE_SIZE;
+        // Add the ASCII value of the character to the hash value
+        hash_value+=name[i];
     }
+    // Modulo the hash value by the size of the hash table
+    hash_value = (hash_value)%TABLE_SIZE;
+    // Return the hash value
     return hash_value;
 }
 
+// Initialize the hash table
 void init_hash_table(){
     for(int i=0; i<TABLE_SIZE; i++){
+        // Set each entry in the hash table to NULL
         hash_table[i]= NULL;
     }
-    // 
 }
 
+// Insert a person into the hash table
+bool hash_table_insert(person *p){
+    // If the person is NULL, return false
+    if(p == NULL) {
+        return false;
+    }
+    // Get the index in the hash table for the person's name
+    int index = hash(p->name);
+    // If the entry in the hash table is not NULL, return false
+    if(hash_table[index] != NULL){
+        return false;
+    }
+    // Insert the person into the hash table
+    hash_table[index] = p;
+    // Return true
+    return true;
+}
+
+// Print the hash table
+void print_hash_table(){
+    for(int i=0; i<TABLE_SIZE; i++){
+        // If the entry in the hash table is NULL, print the index and ---
+        if(hash_table[i] == NULL){
+            printf("\t%i\t---\n", i);
+        }else{
+            // If the entry in the hash table is not NULL, print the index and the person's name
+            printf("\t%i\t%s\n", i, hash_table[i]->name);
+        }
+    }
+}
+
+// Find a person in the hash table
+person *hash_table_find(char *name){
+    // Get the index in the hash table for the person's name
+    int index = hash(name);
+    // If the entry in the hash table is not NULL and the name matches, return the person
+    if(hash_table[index] != NULL && strncmp(hash_table[index]->name, name, TABLE_SIZE) == 0){
+        return hash_table[index];
+    }else{
+        // If the person is not found, return NULL
+        return NULL;
+    }
+}
 int main(){
     
-    // char s[MAX_NAME] = "Ashiq";
-    char* ss[] = {"ash", "che", "ashiq", "chester"};
+    char s[MAX_NAME] = "Ashiq";
+    printf("Ash => %u\n",hash("Ash"));
+    printf("Che => %u\n",hash("Ashiq"));
+    printf("Ashiq => %u\n",hash("Ashiq"));
+    printf("Chester => %u\n",hash("Chester"));
+    printf("Amena => %u\n",hash("Amena"));
+    printf("Kabir => %u\n",hash("Kabir"));
+    printf("Jannat => %u\n",hash("Jannat"));
+    printf("Mosharof => %u\n",hash("Mosharof"));
+    printf("Mahi => %u\n",hash("Mahi"));
+    printf("Zihad => %u\n",hash("Zihad"));
+    printf("Ronaldo => %u\n",hash("Ronaldo"));
+
+    char* ss[] = {"Ash", "Che", "Ashiq", "Chester", "Jannat", "Kabir", "Amena"};
+
     int length = sizeof(ss)/sizeof(ss[0]);
     int sizeofarray = sizeof(ss);
     // printf("length: %d\n", length);
@@ -87,10 +155,43 @@ int main(){
     // So, the total size of the ss array is 3 8 = 24 bytes. 
     // This is why sizeof(ss) is 24. The sizeof operator in 
     // C returns the size of the object or type in bytes
-    
-    for(int i=0; i<length; i++){
-        printf("%s\n", ss[i]);
-    }
+    nl;
+    init_hash_table();
+    // print_hash_table();
 
+    person Ash = {.name = "Ash", .age=24};
+    person Che = {.name = "Che", .age=24};
+    person Ashiq = {.name = "Ashiq", .age=24};
+    person Chester = {.name = "Chester", .age=24};
+    person Amena = {.name = "Amena", .age=40};
+    person Kabir = {.name = "Kabir", .age=50};
+    person Jannat = {.name = "Jannat", .age=16};
+    person Mosharof = {.name = "Mosharof", .age=36};
+    person Zihad = {.name = "Zihad", .age=36};
+    person Mahi = {.name = "Mahi", .age=36};
+    person Ronaldo = {.name = "Ronaldo", .age=36};
+
+    hash_table_insert(&Ash);
+    hash_table_insert(&Che);
+    hash_table_insert(&Ashiq);
+    hash_table_insert(&Chester);
+    hash_table_insert(&Kabir);
+    hash_table_insert(&Amena);
+    hash_table_insert(&Jannat);
+    hash_table_insert(&Mosharof);;
+    hash_table_insert(&Zihad);
+    hash_table_insert(&Mahi);
+    hash_table_insert(&Ronaldo);
+
+    print_hash_table();
+
+    nl;
+
+    person *find = hash_table_find("Zihad");
+    if(find == NULL){
+        printf("Not Found\n");
+    }else{
+        printf("Found %s\n", find->name);
+    }
     return 0;
 }
